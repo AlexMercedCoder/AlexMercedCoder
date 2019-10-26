130d1bff7c8c2292cb800aa48501158f59c386d2
Alex Merced <WillCodeforFood@AlexMerced.com>
2019-10-26 09:22:08 -0400

    redid the main page, previous iterations available at index2-index6.html

---
+++ new.js
@@ -0,0 +1,15 @@
+console.log ('hello');
+
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+        
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+          
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
